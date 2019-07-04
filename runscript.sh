f3feaa1428a29382cb4fdfb067a084da1a5cae39
Martin Kuhn <martin.kuhn@trivago.com>
2019-07-04 21:45:16 +0200

    merge

--- runscript.sh
+++ runscript.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 rm -rf /.vnc
-xvfb-daemon-run /opt/IBController/Scripts/DisplayBannerAndLaunch.sh &
+xvfb-run /opt/IBController/Scripts/DisplayBannerAndLaunch.sh &
 # Tail latest in log dir
 sleep 1
 tail -f $(find $LOG_PATH -maxdepth 1 -type f -printf "%T@ %p\n" | sort -n | tail -n 1 | cut -d' ' -f 2-) &
